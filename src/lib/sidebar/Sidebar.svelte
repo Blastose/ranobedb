ac44ba709f33f0ec56b8e7d07eb883b3751d305a
Blastose <blastoisejay@gmail.com>
2022-07-10 11:33:55 -0700

    Add component for sidebar links

--- src/lib/sidebar/Sidebar.svelte
+++ src/lib/sidebar/Sidebar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import SidebarItem from './SidebarItem.svelte';
 
 	export let sidebarShow = true;
 	export let toggleSidebar: () => void;
@@ -7,44 +7,16 @@
 
 <div class="duration-150 ease-in-out {sidebarShow ? '' : '-ml-64'} ">
 	<nav
-		class="flex flex-col overflow-y-scroll overflow-x-hidden h-screen w-64 sticky top-0 bg-[#e4e7ee] drop-shadow-sm
+		class="overflow-y-scroll overflow-x-auto h-screen w-64 sticky top-0 bg-[#e4e7ee] drop-shadow-sm
     "
 	>
-		<button class="hover:bg-[#cdcedd] self-end px-4" on:click={toggleSidebar}>X</button>
-		<a
-			href="/"
-			class="hover:bg-[#cdcedd] {$page.url.pathname === '/' ? 'bg-blue-200 hover:bg-blue-300' : ''}"
-			>Link</a
-		>
-		<a
-			href="/a"
-			class="hover:bg-[#cdcedd] {$page.url.pathname === '/a'
-				? 'bg-blue-200 hover:bg-blue-300'
-				: ''}">Link</a
-		>
-		<a
-			href="/b"
-			class="hover:bg-[#cdcedd] {$page.url.pathname === '/b'
-				? 'bg-blue-200 hover:bg-blue-300'
-				: ''}">Link</a
-		>
-		<a
-			href="/c"
-			class="hover:bg-[#cdcedd] {$page.url.pathname === '/c'
-				? 'bg-blue-200 hover:bg-blue-300'
-				: ''}">Link</a
-		>
-		<a
-			href="/d"
-			class="hover:bg-[#cdcedd] {$page.url.pathname === '/d'
-				? 'bg-blue-200 hover:bg-blue-300'
-				: ''}">Link</a
-		>
-		<a
-			href="/e"
-			class="hover:bg-[#cdcedd] {$page.url.pathname === '/e'
-				? 'bg-blue-200 hover:bg-blue-300'
-				: ''}">Link</a
-		>
+		<div class="px-4 flex flex-col">
+			<button class="hover:bg-[#cdcedd] self-end px-4" on:click={toggleSidebar}>X</button>
+			<SidebarItem text={'Link1'} href={'/'} />
+			<SidebarItem text={'Link1'} href={'/a'} />
+			<SidebarItem text={'Link1'} href={'/b'} />
+			<SidebarItem text={'Link1'} href={'/c'} />
+			<SidebarItem text={'Link1'} href={'/d'} />
+		</div>
 	</nav>
 </div>
---
+++ src/lib/sidebar/SidebarItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let text = '';
+	export let href = '';
+</script>
+
+<div
+	class="hover:bg-[#cdcedd] {$page.url.pathname === href
+		? 'bg-blue-200 hover:bg-blue-300'
+		: ''} rounded-md duration-75"
+>
+	<a {href}>
+		<div class="px-4 py-1">
+			<span>{text}</span>
+		</div>
+	</a>
+</div>
