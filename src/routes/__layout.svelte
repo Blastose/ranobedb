33365269d546e348d58f33433129fc18152a9aa5
Blastose <blastoisejay@gmail.com>
2022-07-11 10:25:32 -0700

    Hide page on initial load to set sidebar status

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,20 +3,35 @@
 	import Header from '$lib/header/Header.svelte';
 	import Sidebar from '$lib/sidebar/Sidebar.svelte';
 	import { windowWidth } from '$lib/windowWidthStore';
+	import { onMount } from 'svelte';
+	import { sidebarOpen } from '$lib/sidebarStore';
+
+	let initialLoad = true;
+
+	onMount(() => {
+		if ($windowWidth <= 1000) {
+			sidebarOpen.set(false);
+		}
+		initialLoad = false;
+	});
 </script>
 
 <svelte:head><title>Light Novel DB</title></svelte:head>
 <svelte:window bind:innerWidth={$windowWidth} />
 
-<!-- Need to put h-screen into conditional since it messes up the layout for some reason -->
-<div class="flex {$windowWidth <= 1000 ? 'h-screen' : ''}">
-	<Sidebar />
+{#if initialLoad}
+	<div class="flex bg-[#fafafa]" />
+{:else}
+	<!-- Need to put h-screen into conditional since it messes up the layout for some reason -->
+	<div class="flex {$windowWidth <= 1000 ? 'h-screen' : ''}">
+		<Sidebar />
 
-	<div class="flex-grow bg-[#fafafa]">
-		<Header />
+		<div class="flex-grow bg-[#fafafa]">
+			<Header />
 
-		<main class="container mx-auto px-8 py-2 duration-150">
-			<slot />
-		</main>
+			<main class="container mx-auto px-8 py-2 duration-150">
+				<slot />
+			</main>
+		</div>
 	</div>
-</div>
+{/if}
