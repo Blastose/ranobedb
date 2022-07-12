b819cc09154fc6aa360bd1a1ca9646ce6418d863
Blastose <blastoisejay@gmail.com>
2022-07-12 15:34:30 -0700

    Add routes for books and book/[id]

---
+++ src/routes/book/[id].svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { supabase } from '$lib/supabaseClient';
+	import { onMount } from 'svelte';
+
+	let book: any = undefined;
+	let image: any = undefined;
+
+	const getBook = async () => {
+		try {
+			let { data: _book, error } = await supabase
+				.from('book')
+				.select('*')
+				.eq('id', $page.params.id);
+			if (error) throw error;
+			console.log(_book);
+			if (_book) {
+				if (_book.length === 1) {
+					book = _book[0];
+				}
+			}
+
+			let { publicURL, error: error2 } = await supabase.storage
+				.from('cover-images')
+				.getPublicUrl(`${book.cover_image_file_name}.jpg`);
+			if (error2) throw error2;
+			image = publicURL;
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
+
+	onMount(getBook);
+</script>
+
+<!-- TODO Don't commit!!!! -->
+
+<svelte:head>
+	{#if book}
+		<title>{book.title}</title>
+	{:else}
+		<title>Ranobe DB</title>
+	{/if}
+</svelte:head>
+
+<div class="">
+	{#if image}
+		<img src={image} alt="Cover image for {book.title}" width="240" height="340" />
+	{/if}
+	{#if book}
+		<h1 class="text-2xl font-bold">{book.title}</h1>
+		<br />
+		<p>{@html book.description}</p>
+	{:else}
+		<p>loading</p>
+	{/if}
+	<br />
+	<p><a href="/books" class="text-blue-500">Back to books</a></p>
+</div>
---
+++ src/routes/books/index.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { supabase } from '$lib/supabaseClient';
+	import { onMount } from 'svelte';
+
+	let books: any = null;
+
+	const getBooks = async () => {
+		try {
+			let { data, error } = await supabase
+				.from('book_info')
+				.select('*')
+				.order('title_romaji', { ascending: true });
+			if (error) throw error;
+			books = data;
+		} catch (error: any) {
+			console.error(error);
+		}
+	};
+
+	onMount(() => {
+		getBooks();
+	});
+</script>
+
+{#if books}
+	{#each books as book}
+		<p>
+			<a href="/book/{book.id}" class="text-blue-600">{book.title}</a>
+			| {book.publisher.join(', ')}
+		</p>
+	{/each}
+{:else}
+	<div><span>Loading</span></div>
+{/if}
+
+<!-- TODO Don't commit!!!! -->
