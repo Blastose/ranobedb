7937d285d30b994204f05e269e4bee2a586431d0
Blastose <blastoisejay@gmail.com>
2022-07-18 15:59:19 -0700

    Redirect user to login page after logout

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,11 @@ import { handleAuth } from '@supabase/auth-helpers-sveltekit';
 import type { GetSession, Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = sequence(...handleAuth());
+export const handle: Handle = sequence(
+	...handleAuth({
+		logout: { returnTo: '/login' }
+	})
+);
 
 export const getSession: GetSession = async (event) => {
 	const { user, accessToken, error } = event.locals;
