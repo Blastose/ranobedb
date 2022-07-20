1d879f772eefd42fea62ed9bdf2b74f14fa43dc8
Blastose <blastoisejay@gmail.com>
2022-07-20 16:10:38 -0700

    Add error message for invalid login credentials

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Login from '$lib/login/Login.svelte';
 	import { session } from '$app/stores';
+
+	export let error: string = '';
 </script>
 
 {#if !$session.user}
-	<Login />
+	<Login {error} />
 {:else}
 	<h1>Hello {$session.user.email}</h1>
 	<span>You are already logged in!</span>
