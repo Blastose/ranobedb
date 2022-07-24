f4935c73a6c121cb1b1a87e8c86ed8dc7a701b9d
Blastose <blastoisejay@gmail.com>
2022-07-24 11:30:34 -0700

    Cutoff book descriptions that exceed a certain width

--- src/lib/book/BookCard.svelte
+++ src/lib/book/BookCard.svelte
@@ -20,17 +20,15 @@
 <div
 	class="bg-[#e4e7ee] p-2 rounded-sm grid grid-cols-[1fr_75%] md:grid-cols-[150px_1fr] gap-x-2 shadow-sm"
 >
-	<div class="row-span-2">
-		<a href="/book/{book.id}" sveltekit:prefetch>
-			<img class="shadow-sm rounded-sm" src={coverUrl} alt="Cover image for {book.title}" />
-		</a>
-	</div>
-	<div class="flex flex-col gap-2">
+	<a href="/book/{book.id}" sveltekit:prefetch>
+		<img class="shadow-sm rounded-sm" src={coverUrl} alt="Cover image for {book.title}" />
+	</a>
+	<div class="flex flex-col gap-2 ">
 		<a href="/book/{book.id}" sveltekit:prefetch>
 			<span class="font-bold text-xl">{book.title}</span>
 		</a>
-		<div class="text-sm text-[#263147]">
+		<p class="overflow-hidden min-h-min max-h-[100px] sm:max-h-[200px] text-sm text-[#263147]">
 			{@html book.description}
-		</div>
+		</p>
 	</div>
 </div>
