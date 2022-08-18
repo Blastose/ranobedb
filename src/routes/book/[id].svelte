aad95b543a17248712f28a87ccd1b0da0dc09ed2
Blastose <blastoisejay@gmail.com>
2022-08-18 12:03:59 -0700

    Change book layout

--- src/routes/book/[id].svelte
+++ src/routes/book/[id].svelte
@@ -26,7 +26,7 @@
 <main>
 	<div
 		class="bg-image h-24 md:h-48"
-		style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)),
+		style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.6)),
 	url({image});"
 	>
 		<div class="backdrop-blur w-full h-full flex flex-col-reverse">
@@ -39,25 +39,32 @@
 	</div>
 
 	<div class="container mx-auto px-8 py-4 duration-150">
-		<img src={image} alt="Cover image for {book.title}" width="240" height="340" />
-		<br />
-		<h1 class="text-2xl font-bold">{book.title}</h1>
-		<br />
-		<p>{@html book.description}</p>
+		<div class="flex flex-col md:flex-row gap-2">
+			<img
+				src={image}
+				alt="Cover image for {book.title}"
+				class="h-fit shadow-md rounded-sm"
+				width="240"
+				height="340"
+			/>
 
-		<br />
-		{#if $session.user}
-			<button
-				on:click={showModal}
-				class="rounded-md bg-slate-500 hover:bg-slate-600 text-lg px-12 py-2 text-white"
-			>
-				{#if readingStatus}
-					{readingStatus}
-				{:else}
-					Add
+			<div class="flex flex-col gap-4 p-4">
+				{#if $session.user}
+					<button
+						on:click={showModal}
+						class="w-fit rounded-md bg-slate-500 hover:bg-slate-600 text-lg px-12 py-2 text-white"
+					>
+						{#if readingStatus}
+							{readingStatus}
+						{:else}
+							Add
+						{/if}
+					</button>
 				{/if}
-			</button>
-		{/if}
+
+				<p>{@html book.description}</p>
+			</div>
+		</div>
 	</div>
 </main>
 
