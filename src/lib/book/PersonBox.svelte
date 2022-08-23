2620b2b33612f3eed1b21ab142b99cc5608b3d57
Blastose <blastoisejay@gmail.com>
2022-08-22 18:06:52 -0700

    Add check for if person is null

--- src/lib/book/PersonBox.svelte
+++ src/lib/book/PersonBox.svelte
@@ -4,17 +4,21 @@
 	export let type: string;
 </script>
 
-<div
-	class="bg-[#cfd7e7] hover:bg-[#697596] duration-75 hover:text-white rounded-sm shadow-sm text-sm"
->
-	<a href="/person/{person.id}">
-		<div class="px-2 py-1 flex gap-1 items-center justify-center">
-			{#if type === 'artist'}
-				<span><Icon height="20" width="20" name="palette" /></span>
-			{:else if type === 'author'}
-				<span><Icon height="20" width="20" name="pencil" /></span>
-			{/if}
-			<span>{person.name}</span>
-		</div>
-	</a>
-</div>
+<!-- Database query can return a null person. Ideally, the array should be empty, but since it isn't we need to exclude the result -->
+<!-- Can remove this check when we update the query -->
+{#if person.id !== null}
+	<div
+		class="bg-[#cfd7e7] hover:bg-[#697596] duration-75 hover:text-white rounded-sm shadow-sm text-sm"
+	>
+		<a href="/person/{person.id}">
+			<div class="px-2 py-1 flex gap-1 items-center justify-center">
+				{#if type === 'artist'}
+					<span><Icon height="20" width="20" name="palette" /></span>
+				{:else if type === 'author'}
+					<span><Icon height="20" width="20" name="pencil" /></span>
+				{/if}
+				<span>{person.name}</span>
+			</div>
+		</a>
+	</div>
+{/if}
