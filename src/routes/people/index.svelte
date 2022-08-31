9f96f34d8c297e417f8097cacca01a0815f6d150
Blastose <blastoisejay@gmail.com>
2022-08-31 10:09:46 -0700

    Adjust font sizes

--- src/routes/people/index.svelte
+++ src/routes/people/index.svelte
@@ -4,13 +4,16 @@
 </script>
 
 <main class="container mx-auto px-8 pt-4 pb-8 duration-150">
-	<div class="grid grid-cols-2 md:grid-cols-3">
-		{#each people as person (person.person_id)}
-			<div>
-				<a href="/person/{person.person_id}" sveltekit:prefetch>
-					<span>{person.person_name}</span>
-				</a>
-			</div>
-		{/each}
+	<div class="flex flex-col gap-2">
+		<span class="font-bold text-2xl">People</span>
+		<div class="grid grid-cols-2 md:grid-cols-3">
+			{#each people as person (person.person_id)}
+				<div>
+					<a href="/person/{person.person_id}" sveltekit:prefetch>
+						<span>{person.person_name}</span>
+					</a>
+				</div>
+			{/each}
+		</div>
 	</div>
 </main>
