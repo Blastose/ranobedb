70f14fda8202e6307dd015d4f8b7926823c4ce17
Blastose <blastoisejay@gmail.com>
2022-08-31 11:12:19 -0700

    Add publisher pages (#44)
    * Add pages for publishers
    
    * Add heading to books page
    
    * Increase grid gap

--- src/routes/people/index.svelte
+++ src/routes/people/index.svelte
@@ -10,7 +10,7 @@
 <main class="container mx-auto px-8 pt-4 pb-8 duration-150">
 	<div class="flex flex-col gap-2">
 		<span class="font-bold text-2xl">People</span>
-		<div class="grid grid-cols-2 md:grid-cols-3 gap-1">
+		<div class="grid grid-cols-2 md:grid-cols-3 gap-2">
 			{#each people as person (person.person_id)}
 				<div>
 					<a href="/person/{person.person_id}" sveltekit:prefetch>
