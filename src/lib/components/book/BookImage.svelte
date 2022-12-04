46908b1c500930ef082aa17afa3dce5769bd4391
Blastose <blastoisejay@gmail.com>
2022-12-03 16:42:22 -0800

    Add `BookImage` components

---
+++ src/lib/components/book/BookImage.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { PUBLIC_IMAGE_URL } from '$env/static/public';
+	import type { BookInfo } from '$lib/types/dbTypes';
+
+	export let book: BookInfo;
+</script>
+
+<div class="book-image">
+	<a class="h-min" href="/book/{book.id}">
+		<div class="flex flex-col gap-1">
+			<div class="image-container">
+				<img
+					loading="lazy"
+					class="image rounded-sm"
+					src="{PUBLIC_IMAGE_URL}/{book.cover_image_file_name}.jpg"
+					width="240px"
+					height="343px"
+					alt="Cover for {book.title}"
+				/>
+			</div>
+			<p class="title">{book.title}</p>
+		</div>
+	</a>
+</div>
+
+<style>
+	.image-container {
+		overflow: hidden;
+	}
+
+	.title {
+		overflow: hidden;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
+		transition-duration: 150ms;
+	}
+
+	.image {
+		transition-duration: 150ms;
+	}
+
+	.image:hover {
+		transform: scale(1.02);
+	}
+</style>
---
+++ src/lib/components/book/BookImageContainer.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { BookInfo } from '$lib/types/dbTypes';
+	import BookImage from '$lib/components/book/BookImage.svelte';
+
+	export let books: BookInfo[];
+</script>
+
+<div class="book-image-container">
+	{#each books as book (book.id)}
+		<BookImage {book} />
+	{/each}
+</div>
+
+<style>
+	.book-image-container {
+		display: grid;
+		gap: 0.5rem;
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+	}
+
+	@media (min-width: 640px) {
+		.book-image-container {
+			grid-template-columns: repeat(4, minmax(0, 1fr));
+		}
+	}
+	@media (min-width: 768px) {
+		.book-image-container {
+			grid-template-columns: repeat(5, minmax(0, 1fr));
+		}
+	}
+	@media (min-width: 1280px) {
+		.book-image-container {
+			grid-template-columns: repeat(6, minmax(0, 1fr));
+		}
+	}
+	@media (min-width: 1536px) {
+		.book-image-container {
+			grid-template-columns: repeat(8, minmax(0, 1fr));
+		}
+	}
+</style>
