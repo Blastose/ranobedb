9ec652f73080c9a09f49cff9685d1d2a413754e6
Blastose <blastoisejay@gmail.com>
2022-12-13 14:52:54 -0800

    Hide some book cards on smaller screens

--- src/lib/components/book/BookCardSimpleContainer.svelte
+++ src/lib/components/book/BookCardSimpleContainer.svelte
@@ -3,12 +3,14 @@
 	import BookCardSimple from '$lib/components/book/BookCardSimple.svelte';
 
 	export let books: BookInfo[];
-	books = books.slice(0, 16);
 </script>
 
 <div class="book-simple-container">
-	{#each books as book (book.id)}
-		<BookCardSimple {book} />
+	{#each books as book, i (book.id)}
+		<!-- Hide cards over a certain count on smaller screens with css -->
+		<div class="{i < 4 ? 'zero' : ''}{i >= 4 && i < 8 ? 'four' : ''}{i >= 8 ? 'eight' : ''}">
+			<BookCardSimple {book} />
+		</div>
 	{/each}
 </div>
 
@@ -17,10 +19,22 @@
 		column-count: 1;
 	}
 
+	.eight {
+		display: none;
+	}
+
 	@media (min-width: 640px) {
 		.book-simple-container {
 			grid-template-columns: repeat(2, minmax(0, 1fr));
 			column-count: 2;
+		}
+
+		.four {
+			display: block;
+		}
+
+		.eight {
+			display: block;
 		}
 	}
 
