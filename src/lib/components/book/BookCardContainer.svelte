44e42e50310340072ac77ab96676824eb3aa5b74
Blastose <blastoisejay@gmail.com>
2022-12-17 19:54:32 -0800

    Add `BookCardContainer` component

---
+++ src/lib/components/book/BookCardContainer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import BookCard from '$lib/components/book/BookCard.svelte';
+	import type { BookInfo } from '$lib/types/dbTypes';
+
+	export let books: BookInfo[];
+</script>
+
+<div class="book-card-container">
+	{#each books as book}
+		<BookCard {book} />
+	{/each}
+</div>
+
+<style>
+	.book-card-container {
+		display: grid;
+		row-gap: 0.375rem;
+		column-gap: 0.5rem;
+		grid-template-columns: repeat(1, minmax(0, 1fr));
+	}
+
+	@media (min-width: 1024px) {
+		.book-card-container {
+			grid-template-columns: repeat(2, minmax(0, 1fr));
+		}
+	}
+</style>
