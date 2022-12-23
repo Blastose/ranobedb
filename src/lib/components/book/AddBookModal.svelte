de958005689c3857abbc8d457ee39f681e6f3bcf
Blastose <blastoisejay@gmail.com>
2022-12-22 19:54:21 -0800

    Adjust styling of `AddBookModal` component

--- src/lib/components/book/AddBookModal.svelte
+++ src/lib/components/book/AddBookModal.svelte
@@ -21,108 +21,143 @@
 </script>
 
 <div class="flex flex-col gap-2">
-	<span class="text-xl dark:text-white">Add book to reading list</span>
-	<div class="grid grid-rows-[min-content,_1fr] md:grid-cols-[35%,_1fr] gap-y-2 md:gap-x-4">
-		<div class="grid grid-cols-[25%,_1fr] md:grid-cols-none md:grid-rows-[1fr,_min-content] gap-4">
-			<img
-				class="shadow-sm rounded-sm"
-				src="{PUBLIC_IMAGE_URL}/{book.cover_image_file_name}.jpg"
-				alt=""
-			/>
-			<span class="md:hidden text-lg font-semibold dark:text-white">{book.title}</span>
-		</div>
+	<p class="text-xl">Add book to reading list</p>
+	<div class="layout">
+		<p class="title">{book.title}</p>
+		<img class="image" src="{PUBLIC_IMAGE_URL}/{book.cover_image_file_name}.jpg" alt="" />
+		<form
+			class="form"
+			method="POST"
+			action="/api/book/{book.id}/add"
+			use:enhance={() => {
+				return async () => {
+					modal.set(false);
+					invalidateAll();
+				};
+			}}
+		>
+			<div class="flex flex-col gap-1">
+				<label for="startDate">Start date</label>
+				<input class="input" type="date" name="startDate" id="startDate" bind:value={startDate} />
 
-		<div class="flex flex-grow flex-col gap-4">
-			<!-- This span is repeated above but hidden depending on screen size. -->
-			<!-- There might be a way to accomplish this with grid template areas -->
-			<span class="hidden md:block text-lg font-semibold dark:text-white">{book.title}</span>
-			<form
-				class="flex flex-col gap-4"
-				method="POST"
-				action="/api/book/{book.id}/add"
-				use:enhance={() => {
-					return async ({ result }) => {
-						console.log(result);
-						modal.set(false);
-						invalidateAll();
-					};
-				}}
-			>
-				<div class="flex flex-col gap-1">
-					<label for="startDate" class="dark:text-white">Start date</label>
-					<input
-						class="bg-slate-200 p-2 rounded-md dark:text-white dark:bg-dark-500"
-						type="date"
-						name="startDate"
-						id="startDate"
-						bind:value={startDate}
-					/>
-					<label for="finishDate" class="dark:text-white">Finish date</label>
-					<input
-						class="bg-slate-200 p-2 rounded-md dark:text-white dark:bg-dark-500"
-						type="date"
-						name="finishDate"
-						id="finishDate"
-						bind:value={finishDate}
-					/>
+				<label for="finishDate">Finish date</label>
+				<input
+					class="input"
+					type="date"
+					name="finishDate"
+					id="finishDate"
+					bind:value={finishDate}
+				/>
 
-					<label for="label" class="dark:text-white">Status</label>
-					<select
-						bind:value={statusOption}
-						class="bg-slate-200 p-2 rounded-md dark:text-white dark:bg-dark-500"
-						name="label"
-						id="label"
+				<label for="label">Status</label>
+				<select bind:value={statusOption} class="input" name="label" id="label">
+					{#each labels as label (label.label_id)}
+						<option selected={label.label_name === status} value={label.label_name}>
+							{label.label_name}
+						</option>
+					{/each}
+				</select>
+			</div>
+
+			<div class="flex flex-col gap-2">
+				{#if status}
+					<button type="submit" name="type" value="update" class="button">Update</button>
+					<button
+						on:click={async (e) => {
+							if (!confirm('Are you sure you want to remove this book from your reading list?')) {
+								e.preventDefault();
+							}
+						}}
+						type="submit"
+						name="type"
+						value="remove"
+						class="button remove-book"
 					>
-						{#each labels as label (label.label_id)}
-							<option selected={label.label_name === status} value={label.label_name}>
-								{label.label_name}
-							</option>
-						{/each}
-					</select>
-				</div>
-
-				<div class="flex flex-col gap-2">
-					{#if status}
-						<button
-							type="submit"
-							name="type"
-							value="update"
-							class="px-8 py-1 rounded-md text-white duration-150 bg-primary-500 hover:bg-primary-800"
-						>
-							Update
-						</button>
-						<button
-							on:click={async (e) => {
-								if (!confirm('Are you sure you want to remove this book from your reading list?')) {
-									e.preventDefault();
-								}
-							}}
-							type="submit"
-							name="type"
-							value="remove"
-							class="
-							px-8 py-1 rounded-md text-white duration-150
-							bg-[#747490] hover:bg-[#5e5e72]
-							dark:bg-[#737387] dark:hover:bg-[#5e5e72]
-							"
-						>
-							Remove
-						</button>
-					{:else}
-						<button
-							type="submit"
-							name="type"
-							value="add"
-							class="px-8 py-1 rounded-md text-white duration-150 bg-primary-500 hover:bg-primary-800"
-						>
-							Add
-						</button>
-					{/if}
-				</div>
-			</form>
-		</div>
+						Remove
+					</button>
+				{:else}
+					<button type="submit" name="type" value="add" class="button">Add</button>
+				{/if}
+			</div>
+		</form>
 	</div>
 </div>
 
 <style>
+	.image {
+		grid-area: image;
+		border-radius: 0.125rem;
+	}
+
+	.title {
+		grid-area: title;
+		font-weight: 600;
+	}
+
+	.form {
+		grid-area: form;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.input {
+		background-color: rgb(226 232 240);
+		border-radius: 0.375rem;
+		padding: 0.5rem;
+	}
+
+	.button {
+		width: 100%;
+		transition-duration: 150ms;
+		border-radius: 0.375rem;
+		padding: 0.25rem 2rem;
+		color: white;
+		background-color: var(--primary-500);
+	}
+
+	.button:hover {
+		background-color: var(--primary-800);
+	}
+
+	.button.remove-book {
+		background-color: #8585a1;
+	}
+
+	.button.remove-book:hover {
+		background-color: #5e5e72;
+	}
+
+	:global(.dark) .button.remove-book {
+		background-color: #737387;
+	}
+
+	:global(.dark) .button.remove-book:hover {
+		background-color: #5e5e72;
+	}
+
+	:global(.dark) .input {
+		background-color: var(--dark-500);
+	}
+
+	.layout {
+		display: grid;
+		grid-template-areas:
+			'image title'
+			'form form';
+		grid-template-columns: 25% 1fr;
+		grid-template-rows: min-content min-content 1fr;
+		column-gap: 1rem;
+		row-gap: 1rem;
+	}
+
+	@media (min-width: 768px) {
+		.layout {
+			grid-template-areas:
+				'image title'
+				'image form';
+			grid-template-columns: 35% 1fr;
+			grid-template-rows: min-content min-content 1fr;
+		}
+	}
 </style>
