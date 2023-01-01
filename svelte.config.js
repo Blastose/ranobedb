f252ea6709d20c24a6114009e729202a13cd49fd
Blastose <blastoisejay@gmail.com>
2023-01-01 15:39:09 -0800

    Use `vitePreprocess` instead of `svelte-preprocess`

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
-		preprocess({
+		vitePreprocess({
 			postcss: true
 		})
 	],
