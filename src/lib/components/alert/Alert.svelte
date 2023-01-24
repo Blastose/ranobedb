98890e8a563029efeb8e91ee7e4c3623dcdb4f24
Blastose <blastoisejay@gmail.com>
2023-01-23 16:15:48 -0800

    Move alert styling from Tailwind to style tag

--- src/lib/components/alert/Alert.svelte
+++ src/lib/components/alert/Alert.svelte
@@ -2,11 +2,26 @@
 	export let type: 'success' | 'error';
 </script>
 
-<div
-	class="
-py-2 px-4 border rounded-md
-{type === 'success' ? 'border-green-400 text-green-800 bg-green-300' : ''}
-{type === 'error' ? 'border-red-400 text-red-900 bg-red-300' : ''}"
->
+<div class="alert" class:success={type === 'success'} class:error={type === 'error'}>
 	<slot />
 </div>
+
+<style>
+	.alert {
+		padding: 0.5rem 1rem;
+		border-radius: 0.375rem;
+		border-width: 1px;
+	}
+
+	.success {
+		border-color: rgb(74 222 128);
+		color: rgb(22 101 52);
+		background-color: rgb(134 239 172);
+	}
+
+	.error {
+		border-color: rgb(248 113 113);
+		color: rgb(127 29 29);
+		background-color: rgb(252 165 165);
+	}
+</style>
--- tests/edit/book/editbook.spec.ts
+++ tests/edit/book/editbook.spec.ts
@@ -4,10 +4,10 @@ test.describe('edit book', () => {
 	test.use({ storageState: 'storage-state/storageStateA.json' });
 
 	test('User can edit book', async ({ page }) => {
-		await page.goto('/book/5059/edit');
+		await page.goto('/book/2/edit');
 		await page.locator('main form button[type="submit"]').click();
 
-		await expect(page.locator('div.bg-green-300')).toHaveText('Edited entry successfully!');
+		await expect(page.locator('div.alert.success')).toHaveText('Edited entry successfully!');
 	});
 
 	test('User can add and remove people relations to book', async ({ page }) => {
@@ -17,7 +17,7 @@ test.describe('edit book', () => {
 		await page.getByRole('button', { name: '語部マサユキ' }).click();
 		await page.locator('select#role3002artist').selectOption({ label: 'author' });
 		await page.locator('main form button[type="submit"]').click();
-		await expect(page.locator('div.bg-green-300')).toHaveText('Edited entry successfully!');
+		await expect(page.locator('div.alert.success')).toHaveText('Edited entry successfully!');
 
 		await page.goto('/book/5059');
 		await expect(page.getByText('語部マサユキ')).toBeVisible();
@@ -25,7 +25,7 @@ test.describe('edit book', () => {
 		await page.goto('/book/5059/edit');
 		await page.locator('label[for="role3002author"] ~ button.remove-button').click();
 		await page.locator('main form button[type="submit"]').click();
-		await expect(page.locator('div.bg-green-300')).toHaveText('Edited entry successfully!');
+		await expect(page.locator('div.alert.success')).toHaveText('Edited entry successfully!');
 
 		await page.goto('/book/5059');
 		await expect(page.getByText('語部マサユキ')).not.toBeVisible();
@@ -37,7 +37,7 @@ test.describe('edit book', () => {
 		await page.locator('select#role1025author').selectOption({ label: 'artist' });
 		await page.locator('main form button[type="submit"]').click();
 
-		await expect(page.locator('div.bg-red-300')).toHaveText(
+		await expect(page.locator('div.alert.error')).toHaveText(
 			'Invalid form entries. Unable to edit!'
 		);
 
@@ -54,7 +54,7 @@ test.describe('edit book invalid permissions', () => {
 		await page.goto('/book/5059/edit');
 		await page.locator('main form button[type="submit"]').click();
 
-		await expect(page.locator('div.bg-red-300')).toHaveText(
+		await expect(page.locator('div.alert.error')).toHaveText(
 			'Insufficient permission. Unable to edit.'
 		);
 	});
