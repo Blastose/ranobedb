72bdb59dad9f930a1888c827b3b35e0e1e3a5ae3
Blastose <blastoisejay@gmail.com>
2023-02-08 11:29:41 -0800

    Add feature to add publishers to database

--- src/routes/add-to-database/+page.svelte
+++ src/routes/add-to-database/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 </script>
 
+<svelte:head><title>Add to Database - RanobeDB</title></svelte:head>
+
 <main class="main-container flex flex-col gap-4">
 	<h1 class="font-bold text-2xl">Add to database</h1>
 	<a class="link" href="/book/add">Add book</a>
 	<a class="link" href="/release/add">Add release</a>
+	<a class="link" href="/publisher/add">Add publisher</a>
 </main>
