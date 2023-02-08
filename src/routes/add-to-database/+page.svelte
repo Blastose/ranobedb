4a35c0bdac18f1e5604fae8890bb805ed3ee4e8b
Blastose <blastoisejay@gmail.com>
2023-02-08 11:01:39 -0800

    Add feature to add books to database

---
+++ src/routes/add-to-database/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<main class="main-container flex flex-col gap-4">
+	<h1 class="font-bold text-2xl">Add to database</h1>
+	<a class="link" href="/book/add">Add book</a>
+</main>
