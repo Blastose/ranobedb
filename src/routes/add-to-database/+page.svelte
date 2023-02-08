85878187739f32c5311caccc67da76c306111f55
Blastose <blastoisejay@gmail.com>
2023-02-08 12:03:53 -0800

    Add feature to add people to database

--- src/routes/add-to-database/+page.svelte
+++ src/routes/add-to-database/+page.svelte
@@ -8,5 +8,6 @@
 	<a class="link" href="/book/add">Add book</a>
 	<a class="link" href="/series/add">Add series</a>
 	<a class="link" href="/release/add">Add release</a>
+	<a class="link" href="/person/add">Add person</a>
 	<a class="link" href="/publisher/add">Add publisher</a>
 </main>
