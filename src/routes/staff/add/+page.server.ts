b5b68502c36a2639a11bed7f8f3dc3c0d837092a
Blastose <blastoisejay@gmail.com>
2024-03-05 15:59:29 -0800

    Add add staff

---
+++ src/routes/staff/add/+page.server.ts
@@ -0,0 +1,72 @@
+import { staffSchema } from '$lib/zod/schema.js';
+import { fail, redirect } from '@sveltejs/kit';
+import { redirect as flashRedirect } from 'sveltekit-flash-message/server';
+import pkg from 'pg';
+const { DatabaseError } = pkg;
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { hasAddPerms } from '$lib/db/permissions';
+import { ChangePermissionError } from '$lib/server/db/errors/errors.js';
+import { addStaff } from '$lib/server/db/staff/actions.js';
+
+export const load = async ({ locals }) => {
+	if (!locals.user) redirect(302, '/login');
+
+	const form = await superValidate(
+		{
+			aliases: [
+				{
+					main_alias: true,
+					name: ''
+				}
+			]
+		},
+		zod(staffSchema),
+		{
+			errors: false
+		}
+	);
+
+	return { form };
+};
+
+export const actions = {
+	default: async ({ request, locals, cookies }) => {
+		if (!locals.user) return fail(401);
+
+		const form = await superValidate(request, zod(staffSchema));
+		if (!hasAddPerms(locals.user)) {
+			return fail(403, { form });
+		}
+
+		console.dir(form, { depth: null });
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		let success = false;
+		let insertedStaffId;
+		try {
+			insertedStaffId = await addStaff({ staff: form.data }, locals.user);
+			success = true;
+		} catch (e) {
+			if (e instanceof DatabaseError) {
+				//
+			} else if (e instanceof ChangePermissionError) {
+				return fail(403, { form });
+			}
+			console.log(e);
+		}
+
+		if (success) {
+			flashRedirect(
+				303,
+				`/staff/${insertedStaffId}`,
+				{ type: 'success', message: 'Successfully added staff!' },
+				cookies
+			);
+		}
+		return fail(400, { form });
+	}
+};
---
+++ src/routes/staff/add/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import StaffForm from '$lib/components/form/staff/StaffForm.svelte';
+	import PageTitle from '$lib/components/layout/PageTitle.svelte';
+
+	export let data;
+</script>
+
+<PageTitle title="Adding staff" />
+
+<main class="container-rndb">
+	<StaffForm staff={undefined} staffForm={data.form} type="edit" user={data.user} />
+</main>
