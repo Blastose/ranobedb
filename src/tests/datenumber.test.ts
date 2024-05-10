744de2ac473792c3fc51acb87001ac9ce1df8a29
Blastose <blastoisejay@gmail.com>
2024-05-10 14:47:30 -0700

    Separate unit tests

--- src/tests/datenumber.test.ts
+++ src/tests/datenumber.test.ts
@@ -1,23 +1,7 @@
 import { DateNumber } from '$lib/components/form/release/releaseDate';
-import { arrayDiff, arrayIntersection } from '$lib/db/array';
 import { describe, it, expect } from 'vitest';
 
-describe('array', () => {
-	it('diffs the array', () => {
-		const diff = arrayDiff([{ id: 1 }, { id: 2 }, { id: 3 }], [{ id: 2 }, { id: 3 }]);
-		expect(diff[0]).toEqual({ id: 1 });
-	});
-
-	it('intersects the array', () => {
-		const diff = arrayIntersection([{ id: 1 }, { id: 2 }, { id: 3 }], [{ id: 2 }, { id: 3 }]);
-		expect(diff).toMatchObject([{ id: 2 }, { id: 3 }]);
-
-		const diff2 = arrayIntersection([{ id: 1 }, { id: 2 }, { id: 5 }], [{ id: 2 }, { id: 6 }]);
-		expect(diff2).toMatchObject([{ id: 2 }]);
-	});
-});
-
-describe.only('date number', () => {
+describe('date number', () => {
 	it('extracts the date', () => {
 		const dateNumber = new DateNumber(20231212);
 		expect(dateNumber.extractYearMonthDay()).toMatchObject({ year: 2023, month: 12, day: 12 });
