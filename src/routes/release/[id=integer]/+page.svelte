d26ee071ffcf912eef40168b3b7c38ed663ab4d9
Blastose <blastoisejay@gmail.com>
2024-05-31 15:15:42 -0700

    Use title display for page titles

--- src/routes/release/[id=integer]/+page.svelte
+++ src/routes/release/[id=integer]/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { PUBLIC_IMAGE_URL } from '$env/static/public';
 	import PageTitle from '$lib/components/layout/PageTitle.svelte';
 	import DbRouteShell from '$lib/components/layout/db/DBRouteShell.svelte';
 	import Release from '$lib/components/release/id/Release.svelte';
 	import { getDisplayPrefsContext, getNameDisplay } from '$lib/display/prefs.js';
-	import { getThemeContext } from '$lib/stores/themeStore.js';
+	import { getBgImageStyle, getThemeContext } from '$lib/stores/themeStore.js';
 
 	export let data;
 
@@ -11,10 +12,16 @@
 
 	const theme = getThemeContext();
 	const diplayPrefs = getDisplayPrefsContext();
+
+	$: firstBookInReleases = release.books.at(0);
+	$: imageUrl = firstBookInReleases?.image?.filename
+		? `${PUBLIC_IMAGE_URL}${firstBookInReleases?.image?.filename}`
+		: null;
+	$: bgImageStyle = getBgImageStyle($theme, imageUrl);
 </script>
 
 <PageTitle title={getNameDisplay({ obj: release, prefs: $diplayPrefs.names })} />
 
-<DbRouteShell theme={$theme} bgImageStyle={null}>
+<DbRouteShell theme={$theme} {bgImageStyle}>
 	<Release isRevision={false} {release} user={data.user} />
 </DbRouteShell>
--- src/routes/series/[id=integer]/+page.svelte
+++ src/routes/series/[id=integer]/+page.svelte
@@ -3,12 +3,16 @@
 	import PageTitle from '$lib/components/layout/PageTitle.svelte';
 	import DbRouteShell from '$lib/components/layout/db/DBRouteShell.svelte';
 	import Series from '$lib/components/series/id/Series.svelte';
+	import { getDisplayPrefsContext, getTitleDisplay } from '$lib/display/prefs.js';
 	import { getBgImageStyle, getThemeContext } from '$lib/stores/themeStore.js';
 
 	export let data;
 	$: series = data.series;
 
 	const theme = getThemeContext();
+
+	const displayPrefs = getDisplayPrefsContext();
+	$: title = getTitleDisplay({ obj: series, prefs: $displayPrefs.title_prefs });
 
 	$: firstBookInSeries = series.books.at(0);
 	$: imageUrl = firstBookInSeries?.image?.filename
@@ -17,7 +21,7 @@
 	$: bgImageStyle = getBgImageStyle($theme, imageUrl);
 </script>
 
-<PageTitle title={series.title ?? ''} />
+<PageTitle {title} />
 
 <DbRouteShell theme={$theme} {bgImageStyle}>
 	<Series isRevision={false} {series} user={data.user} />
