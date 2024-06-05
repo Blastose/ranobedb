2fb953fc67b0c23c3d37c768a05c01eff1b8a92c
Blastose <blastoisejay@gmail.com>
2024-06-05 11:43:22 -0700

    Update book query filtering example

--- src/routes/books/+page.server.ts
+++ src/routes/books/+page.server.ts
@@ -1,6 +1,7 @@
 import { DBBooks } from '$lib/server/db/books/books.js';
 import { db } from '$lib/server/db/db.js';
 import { paginationBuilderExecuteWithCount } from '$lib/server/db/dbHelpers';
+import { DeduplicateJoinsPlugin } from 'kysely';
 
 export const load = async ({ url, locals }) => {
 	const currentPage = Number(url.searchParams.get('page')) || 1;
@@ -10,8 +11,39 @@ export const load = async ({ url, locals }) => {
 	const dbBooks = DBBooks.fromDB(db, user);
 	let k = dbBooks.getBooks();
 	k = k.where('cte_book.hidden', '=', false);
+	const useQuery = true;
 	if (query) {
-		k = k.where('cte_book.title', 'ilike', `%${query}%`);
+		k = k.withPlugin(new DeduplicateJoinsPlugin()).where((eb) =>
+			eb(
+				'cte_book.id',
+				'in',
+				(eb) =>
+					eb
+						.selectFrom('book')
+						.$if(useQuery, (qb) =>
+							qb
+								.innerJoin('book_title', (join) => join.onRef('book_title.book_id', '=', 'book.id'))
+								.where((eb2) =>
+									eb2.or([
+										eb2('book_title.title', 'ilike', `%${query}%`),
+										eb2('book_title.romaji', 'ilike', `%${query}%`),
+									]),
+								),
+						)
+						.select('book.id'),
+				// Example of books with en releases
+				// .$if(useQuery, (qb) =>
+				// 	qb
+				// 		.innerJoin('release_book', (join) =>
+				// 			join.onRef('release_book.book_id', '=', 'book.id'),
+				// 		)
+				// 		.innerJoin('release', (join) =>
+				// 			join.onRef('release.id', '=', 'release_book.release_id'),
+				// 		)
+				// 		.where((eb2) => eb2.or([eb2('release.lang', '=', 'en')])),
+				// )
+			),
+		);
 	}
 
 	const {
