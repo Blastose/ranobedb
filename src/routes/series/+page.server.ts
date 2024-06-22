b122b06e078236f9757c9026c378e2929743a49c
Blastose <blastoisejay@gmail.com>
2024-06-22 13:57:46 -0700

    Fix series title search and limit

--- src/routes/series/+page.server.ts
+++ src/routes/series/+page.server.ts
@@ -2,6 +2,7 @@ import { db } from '$lib/server/db/db.js';
 import { paginationBuilderExecuteWithCount } from '$lib/server/db/dbHelpers.js';
 import { DBSeries } from '$lib/server/db/series/series.js';
 import { pageSchema, qSchema } from '$lib/server/zod/schema.js';
+import { DeduplicateJoinsPlugin } from 'kysely';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 
@@ -15,8 +16,28 @@ export const load = async ({ url, locals }) => {
 	const dbSeries = DBSeries.fromDB(db, locals.user);
 
 	let query = dbSeries.getSeries().where('cte_series.hidden', '=', false);
+	const useQuery = true;
+
 	if (q) {
-		query = query.where('cte_series.title', 'ilike', `%${q}%`);
+		query = query.withPlugin(new DeduplicateJoinsPlugin()).where((eb) =>
+			eb('cte_series.id', 'in', (eb) =>
+				eb
+					.selectFrom('series')
+					.$if(useQuery, (qb) =>
+						qb
+							.innerJoin('series_title', (join) =>
+								join.onRef('series_title.series_id', '=', 'series.id'),
+							)
+							.where((eb2) =>
+								eb2.or([
+									eb2('series_title.title', 'ilike', `%${q}%`),
+									eb2('series_title.romaji', 'ilike', `%${q}%`),
+								]),
+							),
+					)
+					.select('series.id'),
+			),
+		);
 	}
 
 	query = query.orderBy((eb) => eb.fn.coalesce('cte_series.romaji', 'cte_series.title'));
