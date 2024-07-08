08491601e6fdddd30788bce56288a34f11f6b2a0
Blastose <blastoisejay@gmail.com>
2024-07-07 20:05:14 -0700

    Add welcome page

---
+++ src/routes/welcome/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import NoIndex from '$lib/components/layout/NoIndex.svelte';
+	import PageTitle from '$lib/components/layout/PageTitle.svelte';
+
+	export let data;
+</script>
+
+<PageTitle title="Welcome" />
+<NoIndex />
+
+<main class="container-rndb flex flex-col gap-2">
+	<h1 class="text-3xl font-bold">Welcome to RanobeDB!</h1>
+
+	<div class="markdown">
+		<p>Hello {data.user?.username},</p>
+		<p>Welcome to RanobeDB! We are thrilled to have you join our community.</p>
+
+		<section>
+			<h2>What's next?</h2>
+			<ul>
+				<li>
+					<strong>Verify your email:</strong> You can verify your email by going to the
+					<a href="/settings?view=email">email settings page</a>. You will receive a code in your
+					email that you need to input to verify your email
+				</li>
+				<li>
+					<strong>Contribute:</strong> After you have verified your email, you can now start
+					contributing to the database. Be sure to read the
+					<a href="/editing-guidelines">editing guidelines</a> before you make any changes. You can
+					also join the <a href="https://discord.gg/ZeAnhGncFx" target="_blank">Discord server</a> if
+					you have any questions about editing
+				</li>
+				<li>
+					<strong>Customize:</strong> On the
+					<a href="/settings?view=display">display settings page</a>, you can customize the way
+					RanobeDB looks. You can change the title display of books as well as the theme of the site
+				</li>
+				<li><strong>Explore:</strong> Explore the database and all of the information it offers</li>
+			</ul>
+		</section>
+	</div>
+</main>
