1ced846e7a470950422609f09687fba303824590
Blastose <blastoisejay@gmail.com>
2024-08-14 20:34:10 -0700

    Style lang short code

--- src/lib/components/titles/TitlesSection.svelte
+++ src/lib/components/titles/TitlesSection.svelte
@@ -11,12 +11,16 @@
 </script>
 
 <section>
-	<Collapsible open={false}>
+	<Collapsible open={true}>
 		<svelte:fragment slot="summary"><h2 class="font-bold text-lg">Titles</h2></svelte:fragment>
 		<svelte:fragment slot="details">
 			{#each titles as title}
-				<div class="grid grid-cols-[24px_1fr]">
-					<p>{title.lang}</p>
+				<div class="grid grid-cols-[24px_1fr] gap-2">
+					<div class="pt-[3px]">
+						<p class="sub-text">
+							{title.lang}
+						</p>
+					</div>
 
 					<div>
 						<p>{title.title}</p>
@@ -29,3 +33,23 @@
 		>
 	</Collapsible>
 </section>
+
+<style>
+	.sub-text {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: fit-content;
+		font-size: 0.625rem;
+		line-height: 1rem;
+		font-weight: 600;
+		text-transform: uppercase;
+		border-radius: 0.375rem;
+		border-width: 1px;
+		border-color: var(--text-light-sub);
+	}
+
+	:global(.dark) .sub-text {
+		border-color: var(--text-dark-sub);
+	}
+</style>
