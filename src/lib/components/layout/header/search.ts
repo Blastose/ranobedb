db028325eb8b6afbc002456e7d38ad719548aac4
Blastose <21325959+Blastose@users.noreply.github.com>
2024-09-03 19:32:25 -0700

    Add more items to search bar (#156)

--- src/lib/components/layout/header/search.ts
+++ src/lib/components/layout/header/search.ts
@@ -1,5 +1,7 @@
 import type { BooksApi } from '../../../../routes/api/v0/books/+server';
+import type { PublishersApi } from '../../../../routes/api/v0/publishers/+server';
 import type { SeriesApi } from '../../../../routes/api/v0/series/+server';
+import type { StaffApi } from '../../../../routes/api/v0/staff/+server';
 
 export async function search(term: string) {
 	const booksLimit = 5;
@@ -11,7 +13,20 @@ export async function search(term: string) {
 		await fetch(`/api/v0/series?q=${encodeURIComponent(term)}&limit=${booksLimit}`)
 	).json()) as SeriesApi;
 
-	const [books, series] = await Promise.all([booksPromise, seriesPromise]);
+	const publishersPromise = (await (
+		await fetch(`/api/v0/publishers?q=${encodeURIComponent(term)}&limit=${booksLimit}`)
+	).json()) as PublishersApi;
 
-	return { books: books, series: series };
+	const staffPromise = (await (
+		await fetch(`/api/v0/staff?q=${encodeURIComponent(term)}&limit=${booksLimit}`)
+	).json()) as StaffApi;
+
+	const [books, series, publishers, staff] = await Promise.all([
+		booksPromise,
+		seriesPromise,
+		publishersPromise,
+		staffPromise,
+	]);
+
+	return { books, series, publishers, staff };
 }
---
+++ src/routes/api/v0/staff/+server.ts
@@ -0,0 +1,79 @@
+import { db } from '$lib/server/db/db.js';
+import { paginationBuilderExecuteWithCount } from '$lib/server/db/dbHelpers.js';
+import { DBStaff } from '$lib/server/db/staff/staff.js';
+import { pageSchema, qSchema } from '$lib/server/zod/schema.js';
+import { sql } from 'kysely';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+import type { RequestHandler } from './$types';
+import { json } from '@sveltejs/kit';
+
+async function get(params: { url: URL; locals: App.Locals }) {
+	const { url, locals } = params;
+	const page = await superValidate(url, zod(pageSchema));
+	const currentPage = page.data.page;
+	const qS = await superValidate(url, zod(qSchema));
+	const q = qS.data.q;
+
+	const dbStaff = DBStaff.fromDB(db, locals.user);
+
+	let query = dbStaff.getStaff().where('staff.hidden', '=', false);
+	if (q) {
+		query = query
+			.innerJoin('staff_alias as sa2', 'sa2.staff_id', 'staff.id')
+			.select((eb) =>
+				eb.fn
+					.max(
+						eb.fn('greatest', [
+							eb.fn('word_similarity', [eb.val(q), eb.ref('sa2.name')]),
+							eb.fn('word_similarity', [eb.val(q), eb.ref('sa2.romaji')]),
+						]),
+					)
+					.as('sim_score'),
+			)
+			.where((eb) =>
+				eb.or([
+					eb(eb.val(q), sql.raw('<%'), eb.ref('sa2.name')).$castTo<boolean>(),
+					eb(eb.val(q), sql.raw('<%'), eb.ref('sa2.romaji')).$castTo<boolean>(),
+				]),
+			)
+			.having(
+				(eb) =>
+					eb.fn.max(
+						eb.fn('greatest', [
+							eb.fn('word_similarity', [eb.val(q), eb.ref('sa2.name')]),
+							eb.fn('word_similarity', [eb.val(q), eb.ref('sa2.romaji')]),
+						]),
+					),
+				'>',
+				0.3,
+			)
+			.groupBy(['staff.id', 'staff_alias.name', 'staff_alias.romaji'])
+			.orderBy(`sim_score desc`);
+	}
+
+	query = query.orderBy((eb) => eb.fn.coalesce('staff_alias.romaji', 'staff_alias.name'));
+
+	const {
+		result: staff,
+		count,
+		totalPages,
+	} = await paginationBuilderExecuteWithCount(query, {
+		limit: 5,
+		page: currentPage,
+	});
+
+	return {
+		staff,
+		count,
+		currentPage,
+		totalPages,
+	};
+}
+
+export type StaffApi = Awaited<ReturnType<typeof get>>;
+
+export const GET: RequestHandler = async ({ url, locals }) => {
+	return json(await get({ url, locals }));
+};
