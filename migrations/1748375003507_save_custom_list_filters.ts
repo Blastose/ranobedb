d7793a18d41ee369069753c2ec9acd9e02c8acc1
Blastose <blastoisejay@gmail.com>
2025-05-28 15:23:45 -0700

    Fix spacing

--- migrations/1748375003507_save_custom_list_filters.ts
+++ migrations/1748375003507_save_custom_list_filters.ts
@@ -5,7 +5,7 @@ export async function up(db: Kysely<unknown>): Promise<void> {
 CREATE TABLE public.saved_filter (
     user_id text NOT NULL,
     item_name db_item NOT NULL,
-		filters text NOT NULL,
+    filters text NOT NULL,
     FOREIGN KEY (user_id) REFERENCES public.auth_user(id),
     PRIMARY KEY (user_id, item_name)
 );
