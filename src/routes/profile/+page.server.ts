321fed06760792f3fe4bd384fdde4e68a19d89bc
Blastose <blastoisejay@gmail.com>
2025-06-02 13:56:09 -0700

    Remove unused route

--- src/routes/profile/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { buildRedirectUrl } from '$lib/utils/url.js';
-import { redirect } from '@sveltejs/kit';
-
-export const load = async ({ locals, url }) => {
-	if (!locals.user) {
-		redirect(302, buildRedirectUrl(url, '/login'));
-	}
-};
