f6a4acd81fa992347ed7edcc603e7f6035de4d3e
Blastose <blastoisejay@gmail.com>
2026-03-16 16:11:11 -0700

    Fix default

--- migrations/1773699809586_add_label_badge_field_to_auth_user.ts
+++ migrations/1773699809586_add_label_badge_field_to_auth_user.ts
@@ -3,7 +3,7 @@ import { sql, type Kysely } from 'kysely';
 export async function up(db: Kysely<unknown>): Promise<void> {
 	await sql`
 ALTER TABLE public.auth_user
-ALTER COLUMN display_prefs SET DEFAULT '{"names": "romaji","title_prefs": [ { "lang": "en", "romaji": false }, { "lang": "ja", "romaji": true } ],"descriptions": "en","label_badge_display": ""}'::jsonb;
+ALTER COLUMN display_prefs SET DEFAULT '{"names": "romaji","title_prefs": [ { "lang": "en", "romaji": false }, { "lang": "ja", "romaji": true } ],"descriptions": "en","label_badge_display": true}'::jsonb;
 
 UPDATE public.auth_user
 SET display_prefs['label_badge_display'] = to_jsonb(true);
