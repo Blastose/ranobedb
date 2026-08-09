6389cf4f8d7f7f21c2eb05dbe9da038d8aa5c416
anispwyn <anisphia_wynn_palettia@proton.me>
2026-08-09 13:15:14 +0700

    chore: remove redundant return

--- src/routes/api/v0/user/me/+server.ts
+++ src/routes/api/v0/user/me/+server.ts
@@ -2,8 +2,8 @@ import type { RequestHandler } from './$types';
 import { error, json } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ locals }) => {
-	if (!locals.user) {
-		return error(401, { message: 'unauthorized' });
-	}
-	return json(locals.user);
+  if (!locals.user) {
+    error(401, { message: 'unauthorized' });
+  }
+  return json(locals.user);
 };
