a65ef486802df0075367e7edd88256d92e8773a2
anispwyn <anisphia_wynn_palettia@proton.me>
2026-08-12 04:20:21 +0700

    chore: move repeated staffActions into a class

--- src/routes/api/i/user/staff/[id=integer]/+page.server.ts
+++ src/routes/api/i/user/staff/[id=integer]/+page.server.ts
@@ -3,51 +3,8 @@ import type { Actions } from './$types';
 import { superValidate, message } from 'sveltekit-superforms';
 import { zod4 } from 'sveltekit-superforms/adapters';
 import { userListStaffSchema } from '$lib/server/zod/schema';
-import type {
-	Language,
-	DB,
-	ReleaseFormat,
-	UserListStaffLang,
-	UserListStaffFormat,
-} from '$lib/server/db/dbTypes';
-import type { Insertable, Transaction } from 'kysely';
 import { db } from '$lib/server/db/db';
-
-async function addUserListStaffLangsAndFormats(params: {
-	staff_id: number;
-	user_id: string;
-	langs: Language[];
-	formats: ReleaseFormat[];
-	trx: Transaction<DB>;
-}) {
-	const { trx } = params;
-	await trx
-		.deleteFrom('user_list_staff_lang')
-		.where('user_list_staff_lang.staff_id', '=', params.staff_id)
-		.where('user_list_staff_lang.user_id', '=', params.user_id)
-		.execute();
-	await trx
-		.deleteFrom('user_list_staff_format')
-		.where('user_list_staff_format.staff_id', '=', params.staff_id)
-		.where('user_list_staff_format.user_id', '=', params.user_id)
-		.execute();
-	const userListstaffLangs = params.langs.map((v) => ({
-		lang: v,
-		staff_id: params.staff_id,
-		user_id: params.user_id,
-	})) satisfies Insertable<UserListStaffLang>[];
-	if (userListstaffLangs.length > 0) {
-		await trx.insertInto('user_list_staff_lang').values(userListstaffLangs).execute();
-	}
-	const userListstaffFormats = params.formats.map((v) => ({
-		format: v,
-		staff_id: params.staff_id,
-		user_id: params.user_id,
-	})) satisfies Insertable<UserListStaffFormat>[];
-	if (userListstaffFormats.length > 0) {
-		await trx.insertInto('user_list_staff_format').values(userListstaffFormats).execute();
-	}
-}
+import { DbStaffActions } from '$lib/server/db/user/staff-list';
 
 export const actions = {
 	default: async ({ params, request, locals }) => {
@@ -63,74 +20,34 @@ export const actions = {
 		}
 
 		let messageText = '';
-
+		const dbStaffActions = new DbStaffActions(db);
 		try {
 			if (form.data.type === 'add') {
-				await db.transaction().execute(async (trx) => {
-					await trx
-						.insertInto('user_list_staff')
-						.values({
-							notify_book: true,
-							show_upcoming: true,
-							only_first_book: form.data.only_first_book,
-							staff_id: staffId,
-							user_id: user.id,
-						})
-						.execute();
-					await addUserListStaffLangsAndFormats({
-						formats: form.data.formats,
-						langs: form.data.langs,
-						staff_id: staffId,
-						user_id: user.id,
-						trx: trx,
-					});
+				await dbStaffActions.addStaffToList({
+					notify_book: form.data.notify_book,
+					show_upcoming: form.data.show_upcoming,
+					only_first_book: form.data.only_first_book,
+					staff_id: staffId,
+					user_id: user.id,
+					formats: form.data.formats,
+					langs: form.data.langs,
 				});
 				messageText = 'Followed staff!';
 			} else if (form.data.type === 'update') {
-				await db.transaction().execute(async (trx) => {
-					await trx
-						.insertInto('user_list_staff')
-						.values({
-							notify_book: true,
-							show_upcoming: true,
-							only_first_book: form.data.only_first_book,
-							staff_id: staffId,
-							user_id: user.id,
-						})
-						.onConflict((oc) =>
-							oc.columns(['staff_id', 'user_id']).doUpdateSet({
-								notify_book: form.data.notify_book,
-								show_upcoming: form.data.show_upcoming,
-								only_first_book: form.data.only_first_book,
-							}),
-						)
-						.execute();
-					await addUserListStaffLangsAndFormats({
-						formats: form.data.formats,
-						langs: form.data.langs,
-						staff_id: staffId,
-						user_id: user.id,
-						trx: trx,
-					});
+				await dbStaffActions.editStaffInList({
+					notify_book: form.data.notify_book,
+					show_upcoming: form.data.show_upcoming,
+					only_first_book: form.data.only_first_book,
+					staff_id: staffId,
+					user_id: user.id,
+					formats: form.data.formats,
+					langs: form.data.langs,
 				});
 				messageText = 'Updated staff follow settings successfully!';
 			} else if (form.data.type === 'delete') {
-				await db.transaction().execute(async (trx) => {
-					await trx
-						.deleteFrom('user_list_staff_format')
-						.where('user_list_staff_format.staff_id', '=', staffId)
-						.where('user_list_staff_format.user_id', '=', user.id)
-						.execute();
-					await trx
-						.deleteFrom('user_list_staff_lang')
-						.where('user_list_staff_lang.staff_id', '=', staffId)
-						.where('user_list_staff_lang.user_id', '=', user.id)
-						.execute();
-					await trx
-						.deleteFrom('user_list_staff')
-						.where('user_list_staff.staff_id', '=', staffId)
-						.where('user_list_staff.user_id', '=', user.id)
-						.execute();
+				await dbStaffActions.removeStaffInList({
+					staff_id: staffId,
+					user_id: user.id,
 				});
 				messageText = 'Unfollowed staff!';
 			}
--- src/routes/api/v0/user/staff/[id=integer]/+server.ts
+++ src/routes/api/v0/user/staff/[id=integer]/+server.ts
@@ -2,55 +2,11 @@ import { error, json, type RequestEvent, type RequestHandler } from '@sveltejs/k
 import { userListStaffSchema } from '$lib/server/zod/schema';
 import * as z from 'zod';
 import { db } from '$lib/server/db/db';
-import type {
-	DB,
-	Language,
-	ReleaseFormat,
-	UserListStaffFormat,
-	UserListStaffLang,
-} from '$lib/server/db/dbTypes';
-import type { Insertable, Transaction } from 'kysely';
+import { DbStaffActions } from '$lib/server/db/user/staff-list';
 
 const apiUserListStaffSchema = userListStaffSchema.omit({
 	type: true,
 });
-
-// a copy of /i/user/staff/[id=integer]/+page_server.ts
-async function addUserListStaffLangsAndFormats(params: {
-	staff_id: number;
-	user_id: string;
-	langs: Language[];
-	formats: ReleaseFormat[];
-	trx: Transaction<DB>;
-}) {
-	const { trx } = params;
-	await trx
-		.deleteFrom('user_list_staff_lang')
-		.where('user_list_staff_lang.staff_id', '=', params.staff_id)
-		.where('user_list_staff_lang.user_id', '=', params.user_id)
-		.execute();
-	await trx
-		.deleteFrom('user_list_staff_format')
-		.where('user_list_staff_format.staff_id', '=', params.staff_id)
-		.where('user_list_staff_format.user_id', '=', params.user_id)
-		.execute();
-	const userListstaffLangs = params.langs.map((v) => ({
-		lang: v,
-		staff_id: params.staff_id,
-		user_id: params.user_id,
-	})) satisfies Insertable<UserListStaffLang>[];
-	if (userListstaffLangs.length > 0) {
-		await trx.insertInto('user_list_staff_lang').values(userListstaffLangs).execute();
-	}
-	const userListstaffFormats = params.formats.map((v) => ({
-		format: v,
-		staff_id: params.staff_id,
-		user_id: params.user_id,
-	})) satisfies Insertable<UserListStaffFormat>[];
-	if (userListstaffFormats.length > 0) {
-		await trx.insertInto('user_list_staff_format').values(userListstaffFormats).execute();
-	}
-}
 
 async function validateRequest(event: RequestEvent) {
 	const { params, locals } = event;
@@ -100,53 +56,18 @@ async function addOrEditUserListStaff(event: RequestEvent) {
 		error(400, 'Staff does not exist');
 	}
 
-	// transaction is also copied from /i/user/staff/[id=integer]/+page_server.ts
+	const dbStaffActions = new DbStaffActions(db);
 	if (!existingUserStaff) {
-		await db.transaction().execute(async (trx) => {
-			await trx
-				.insertInto('user_list_staff')
-				.values({
-					notify_book: data.notify_book,
-					show_upcoming: data.show_upcoming,
-					only_first_book: data.only_first_book,
-					staff_id,
-					user_id: user.id,
-				})
-				.execute();
-			await addUserListStaffLangsAndFormats({
-				formats: data.formats,
-				langs: data.langs,
-				staff_id,
-				user_id: user.id,
-				trx: trx,
-			});
+		await dbStaffActions.addStaffToList({
+			user_id: user.id,
+			staff_id,
+			...data,
 		});
 	} else {
-		await db.transaction().execute(async (trx) => {
-			await trx
-				.insertInto('user_list_staff')
-				.values({
-					notify_book: data.notify_book,
-					show_upcoming: data.show_upcoming,
-					only_first_book: data.only_first_book,
-					staff_id,
-					user_id: user.id,
-				})
-				.onConflict((oc) =>
-					oc.columns(['staff_id', 'user_id']).doUpdateSet({
-						notify_book: data.notify_book,
-						show_upcoming: data.show_upcoming,
-						only_first_book: data.only_first_book,
-					}),
-				)
-				.execute();
-			await addUserListStaffLangsAndFormats({
-				formats: data.formats,
-				langs: data.langs,
-				staff_id,
-				user_id: user.id,
-				trx: trx,
-			});
+		await dbStaffActions.editStaffInList({
+			user_id: user.id,
+			staff_id,
+			...data,
 		});
 	}
 	return json({ success: true, action: existingUserStaff ? 'edit' : 'add' });
@@ -164,25 +85,12 @@ async function deleteUserListStaff(event: RequestEvent) {
 	if (!existingUserStaff) {
 		error(400, 'Staff is not in the list');
 	}
-	// transaction is also copied from /i/user/staff/[id=integer]/+page_server.ts
-	await db.transaction().execute(async (trx) => {
-		await trx
-			.deleteFrom('user_list_staff_format')
-			.where('user_list_staff_format.staff_id', '=', staff_id)
-			.where('user_list_staff_format.user_id', '=', user.id)
-			.execute();
-		await trx
-			.deleteFrom('user_list_staff_lang')
-			.where('user_list_staff_lang.staff_id', '=', staff_id)
-			.where('user_list_staff_lang.user_id', '=', user.id)
-			.execute();
-		await trx
-			.deleteFrom('user_list_staff')
-			.where('user_list_staff.staff_id', '=', staff_id)
-			.where('user_list_staff.user_id', '=', user.id)
-			.execute();
-	});
 
+	const dbStaffActions = new DbStaffActions(db);
+	await dbStaffActions.removeStaffInList({
+		user_id: user.id,
+		staff_id,
+	});
 	return json({ success: true });
 }
 
